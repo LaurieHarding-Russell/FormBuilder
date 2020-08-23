ccb734f2e65d3040e8ab8e8938fab2c373fbf1c1
Laurie Harding-Russell <laurieHardingRussell@gmail.com>
2020-08-23 15:22:07 -0600

    Added confusing failing test.

---
+++ test/CommonTypesTest/CommonTypeTest.cpp
@@ -0,0 +1,43 @@
+#include "gtest/gtest.h"
+#include "lib/CommonTypes.h"
+
+
+TEST(Common, flipImage) {
+    unsigned char picture [] = {
+        (unsigned char)255, (unsigned char)0, (unsigned char)0,(unsigned char)255,          (unsigned char)0, (unsigned char)255, (unsigned char)0,(unsigned char)255,
+        (unsigned char)0, (unsigned char)0, (unsigned char)255,(unsigned char)255,          (unsigned char)0, (unsigned char)0, (unsigned char)0,(unsigned char)255
+    };
+
+    Texture* texture = new Texture();
+    texture->data = picture;
+    texture->width = 2;
+    texture->height = 2;
+
+    Texture::flipYAxis(texture);
+
+    EXPECT_EQ(texture->data[0], (unsigned char)0);
+    EXPECT_EQ(texture->data[1], (unsigned char)0);
+    EXPECT_EQ(texture->data[2], (unsigned char)255);
+    EXPECT_EQ(texture->data[3], (unsigned char)255);
+
+    EXPECT_EQ(texture->data[4], (unsigned char)0);
+    EXPECT_EQ(texture->data[5], (unsigned char)0);
+    EXPECT_EQ(texture->data[6], (unsigned char)0);
+    EXPECT_EQ(texture->data[7], (unsigned char)255);
+
+    EXPECT_EQ(texture->data[8], (unsigned char)255);
+    EXPECT_EQ(texture->data[9], (unsigned char)0);
+    EXPECT_EQ(texture->data[10], (unsigned char)0);
+    EXPECT_EQ(texture->data[11], (unsigned char)255);
+
+    EXPECT_EQ(texture->data[0], (unsigned char)0);
+    EXPECT_EQ(texture->data[1], (unsigned char)255);
+    EXPECT_EQ(texture->data[2], (unsigned char)0);
+    EXPECT_EQ(texture->data[3], (unsigned char)255);
+
+}
+
+GTEST_API_ int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
